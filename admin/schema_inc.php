97b8c75b0f086a16378d20edd83041848b54c5eb
Max Kremmel <xing@synapse.plus.com>
2006-02-05 21:19:03 +0000

    remove unused parameters in registerPackageInfo

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -32,9 +32,6 @@ foreach( array_keys( $tables ) AS $tableName ) {
 $gBitInstaller->registerPackageInfo( MESSU_PKG_NAME, array(
 	'description' => "An intra-site messaging system for users.",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
-	'version' => '0.1',
-	'state' => 'beta',
-	'dependencies' => '',
 ) );
 
 
