d5e4328cfbae9b0c8b12314886bbc8754c7fc831
Max Kremmel <xing@synapse.plus.com>
2006-12-25 08:23:07 +0000

    fix references to preferences file

--- user_preferences_inc.php
+++ user_preferences_inc.php
@@ -3,13 +3,13 @@
 * message package modules
 *
 * @author   
-* @version  $Header: /cvsroot/bitweaver/_bit_messages/user_preferences_inc.php,v 1.2 2006/10/13 12:45:32 lsces Exp $
+* @version  $Header: /cvsroot/bitweaver/_bit_messages/user_preferences_inc.php,v 1.3 2006/12/25 08:23:07 squareing Exp $
 * @package  messages
 * @subpackage functions
 */
 
 $title = "User Messages";
-if (isset($_REQUEST['messprefs'])) {
+if( isset( $_REQUEST['messprefs'] ) ) {
 	$editUser->storePreference( 'messages_max_records', $_REQUEST['messages_max_records'], 'users' );
 	$editUser->storePreference( 'messages_min_priority', $_REQUEST['messages_min_priority'], 'users' );
 	$editUser->storePreference( 'messages_alert', !empty( $_REQUEST['messages_alert'] ) ? 'y' : 'n', 'users' );
