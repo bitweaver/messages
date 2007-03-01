31e4e90d9d4904c980ed6d59f3fc81bfed7a145a
Max Kremmel <xing@synapse.plus.com>
2007-03-01 09:44:18 +0000

    delete message email entry in liberty_content_prefs if user doesn't want to receive emails

--- user_preferences_inc.php
+++ user_preferences_inc.php
@@ -3,7 +3,7 @@
 * message package modules
 *
 * @author   
-* @version  $Header: /cvsroot/bitweaver/_bit_messages/user_preferences_inc.php,v 1.4 2007/03/01 08:26:16 squareing Exp $
+* @version  $Header: /cvsroot/bitweaver/_bit_messages/user_preferences_inc.php,v 1.5 2007/03/01 09:44:18 squareing Exp $
 * @package  messages
 * @subpackage functions
 */
@@ -12,7 +12,7 @@ $title = "User Messages";
 if( isset( $_REQUEST['messprefs'] ) ) {
 	$editUser->storePreference( 'messages_alert', !empty( $_REQUEST['messages_alert'] ) ? 'y' : 'n', USERS_PKG_NAME );
 	$editUser->storePreference( 'messages_max_records', $_REQUEST['messages_max_records'], USERS_PKG_NAME );
-	$editUser->storePreference( 'messages_min_priority', $_REQUEST['messages_min_priority'], USERS_PKG_NAME );
+	$editUser->storePreference( 'messages_min_priority', !empty( $_REQUEST['messages_min_priority'] ) ? $_REQUEST['messages_min_priority'] : NULL, USERS_PKG_NAME );
 	$editUser->storePreference( 'messages_allow_messages', !empty( $_REQUEST['messages_allow_messages'] ) ? 'y' : 'n', USERS_PKG_NAME );
 }
 ?>
