7843402bc4fd9ea011d88dd7213afaad19550139
wjames5 <will@tekimaki.com>
2009-10-01 14:17:02 +0000

    remove ref to non-existant copyright.txt file

--- read.php
+++ read.php
@@ -3,13 +3,13 @@
 * message package modules
 *
 * @author   
-* @version  $Header: /cvsroot/bitweaver/_bit_messages/read.php,v 1.11 2009/10/01 13:45:44 wjames5 Exp $
+* @version  $Header: /cvsroot/bitweaver/_bit_messages/read.php,v 1.12 2009/10/01 14:17:02 wjames5 Exp $
 * @package  messages
 * @subpackage functions
 */
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// All Rights Reserved. See below for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
 
 /**
